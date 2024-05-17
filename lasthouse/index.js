b2bf04bbf7805d6f67435a9da0a91968b6a3fb5c
renascented <140450870+renasmell@users.noreply.github.com>
2024-05-17 16:42:03 -0400

    Update index.js
    fixes

--- lasthouse/index.js
+++ lasthouse/index.js
@@ -4,7 +4,7 @@ let prefix = '&d>'
 
 const removeRanks = (playername) => playername.replace(/\[(.*)\] ?/g, '');
 const getTabFooter = (formatted) => formatted ? TabList.getFooterMessage().getFormattedText() : TabList.getFooterMessage().getUnformattedText();
-const inHousing = () => /HOUSING/.test(Scoreboard.getTitle())
+const inHousing = () => /hypixel/.test(Server.getIP()) && /HOUSING/.test(Scoreboard.getTitle()) && /You are in (.*), by (.*)/.test(getTabFooter(false))
 
 function getHouseInfo(formatted=false) {
   // house and player name index 1 2
@@ -15,7 +15,7 @@ function getHouseInfo(formatted=false) {
   // housing has weird ascii characters in the scoreboard :P
   let serverID = Scoreboard.getLineByIndex(Scoreboard.getLines().length-1).toString().removeFormatting().replace(/[^\x00-\x7F]/g, '').match(/ m\d+?\w+? /i).join('').replace(/\s/g, '');
 
-  return { name: footer[1], owner: formatted ? footer[2] : removeRanks(footer[2]), id: serverID.replace(/m/i, '') };
+  return { name: footer[2], owner: formatted ? footer[2] : removeRanks(footer[2]), id: serverID.replace(/m/i, '') };
 }
 
 import Promise from "../PromiseV2";
@@ -26,7 +26,7 @@ let last_houses = [];
 register('worldLoad', () => {
   new Promise((resolve, reject) => {
     const loop = () => {
-      if (TabList.getFooterMessage().getUnformattedText() && Scoreboard.getTitle()) return resolve();
+      if (TabList.getFooterMessage()?.getUnformattedText() && Scoreboard.getTitle()) return resolve();
 
       setTimeout(() => loop(), 200);
     }
@@ -41,20 +41,20 @@ register('worldUnload', () => last_houses[0] !== current_house ? last_houses.uns
 register('command', () => {
   if (last_houses.length <= 0) return ChatLib.chat(`${prefix} &cno last house to visit!`);
   ChatLib.command(`visit ${removeRanks(last_houses[0].owner.removeFormatting())} ${last_houses[0].name.removeFormatting()}`, false)
-}).setName('lasthouse');
+}).setName('lasthouse').setAliases("lh");
 
 register('command', () => {
   let list = new Message(ChatLib.getCenteredText("&8===================&6Last Houses&8===================\n"))
 
   function makeHouseComponent(house) {
-    let comp = new TextComponent(`${house.name} by ${house.owner}\n`)
+    let comp = new TextComponent(`&7[${house.id}] ${house.name} by ${house.owner}\n`)
     comp.setClick('run_command', `/visit ${removeRanks(house.owner.removeFormatting())} ${house.name.removeFormatting()}`)
     comp.setHover('show_text', 'visit this house!');
 
     return comp;
   }
 
-  last_houses.forEach(house => list.addTextComponent(makeHouseComponent(house)));
+  last_houses.filter(e => e).forEach(house => list.addTextComponent(makeHouseComponent(house)));
   list.addTextComponent(ChatLib.getCenteredText("&8======================================"))
   list.chat();
-}).setName('lasthouses');
+}).setName('lasthouses').setAliases("lhs");
